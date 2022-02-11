35582a7cdf9303dad8197b18d788a9f632ca853f
evanboylan <65260914+evanboylan@users.noreply.github.com>
2022-02-10 19:41:35 -0500

    Importing initializr files

---
+++ src/test/java/com/example/honeyheist/HoneyheistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honeyheist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyheistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
